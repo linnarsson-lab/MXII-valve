577f9f0a9f0ab5a54202b2a3344ecad8c6acf952
Lars Borm <larsborm@hotmail.com>
2020-03-05 18:20:57 +0100

    Add find_address
    Remove a bunch of ugly old code.

--- MXII_valve.py
+++ MXII_valve.py
@@ -10,8 +10,62 @@
 
 
 import serial
+from serial.tools import list_ports
 import time
 import re
+
+#   FIND SERIAL PORT
+def find_address(identifier = None):
+    """
+    Find the address of a serial device. It can either find the address using
+    an identifier given by the user or by manually unplugging and plugging in 
+    the device.
+    Input:
+    `identifier`(str): Any attribute of the connection. Usually USB to Serial
+        converters use an FTDI chip. These chips store a number of attributes
+        like: name, serial number or manufacturer. This can be used to 
+        identify a serial connection as long as it is unique. See the pyserial
+        list_ports.grep() function for more details.
+    Returns:
+    The function prints the address and serial number of the FTDI chip.
+    `port`(obj): Returns a pyserial port object. port.device stores the 
+        address.
+    
+    """
+    found = False
+    if identifier != None:
+        port = [i for i in list(list_ports.grep(identifier))]
+        
+        if len(port) == 1:
+            print('Device address: {}'.format(port[0].device))
+            found = True
+        elif len(port) == 0:
+            print('''No devices found using identifier: {}
+            \nContinue with manually finding USB address...\n'''.format(identifier))
+        else:
+            for p in connections:
+                print('{:15}| {:15} |{:15} |{:15} |{:15}'.format('Device', 'Name', 'Serial number', 'Manufacturer', 'Description') )
+                print('{:15}| {:15} |{:15} |{:15} |{:15}\n'.format(str(p.device), str(p.name), str(p.serial_number), str(p.manufacturer), str(p.description)))
+            raise Exception("""The input returned multiple devices, see above.""")
+
+    if found == False:
+        print('Performing manual USB address search.')
+        while True:
+            input('    Unplug the USB. Press Enter if unplugged...')
+            before = list_ports.comports()
+            input('    Plug in the USB. Press Enter if USB has been plugged in...')
+            after = list_ports.comports()
+            port = [i for i in after if i not in before]
+            if port != []:
+                break
+            print('    No port found. Try again.\n')
+        print('Device address: {}'.format(port[0].device))
+        try:
+            print('Device serial_number: {}'.format(port[0].serial_number))
+        except Exception:
+            print('Could not find serial number of device.')
+    
+    return port[0]
 
 
 class MX_valve():
@@ -43,6 +97,7 @@ class MX_valve():
         self.ser = serial.Serial(address, timeout = 2, baudrate=19200, 
                    write_timeout=5)
         self.verbose = verbose
+        self.verboseprint = print if self.verbose else lambda *a, **k: None
         #19200 is the default baudrate for the MXII valve. see manual if you
         #want to change this: MX Series II(TM) Driver Development Package,
         #File-141803967
@@ -72,12 +127,11 @@ class MX_valve():
         
         """
         msg = self.message_builder('read')
-        self.read_message() #flush output
+        self.read_message() 
         while True:
             self.write_message(msg)
             response = self.read_message()
-            port_val = re.compile(b'0.\\r')
-            if re.match(port_val, response):
+            if response != b'**':
                 break
 
     def message_builder (self, objective, port = 1):
@@ -113,10 +167,9 @@ class MX_valve():
         Output: response of the pump.
         
         """
-        ser = self.ser
-        n = ser.inWaiting()
+        n = self.ser.inWaiting()
         time.sleep(0.05) #Alow valve to process
-        response = ser.read(n)
+        response = self.ser.read(n)
         time.sleep(0.05) #Alow valve to process
         return response
     
@@ -127,8 +180,10 @@ class MX_valve():
         `message`: Message to sent to valve
             
         """
-        ser = self.ser
-        ser.write(message)
+        #Flush the pre-existing output
+        self.read_message() 
+        #Write new message
+        self.ser.write(message)
         time.sleep(0.05)
     
     def response_interpret(self, response):
@@ -153,63 +208,54 @@ class MX_valve():
             #Here 65 is transformed back to A with chr() and then converted
             #to the right decimal (in this case '10') with int(x, 16)
             current_port = int(chr(response[1]), 16)
-            return True, int(current_port)
+            return int(current_port)
         elif response == b'\r':
             return True
+        # Valve bussy
         elif response == b'*' or response == b'**':
-            print(response)
-            print('I did not test this yet, if you see it confirm it works and change the code')
             return False
-            #I (LEBorm) added this part to interpret the 'bussy' response. However I did not test it.
-            #Response: "*" i.e. "0x2A" for 
-            #"valve is busy". However, the other functions should not allow this to happen.
         else:
             print(''' If you see this message, you found an unknown error,
             Please send me (Lars Borm) a message with the following error code
             and your script. I will try to fix it.''')
             raise ValueError('Unknown valve response: "{}", can not interpret',format(response))
+
+    def get_port(self):
+        """
+        Read the current port position of the valve. 
+        Returns:
+        Current port
+
+        """
+        msg = self.message_builder('read')
+        self.write_message(msg)
+        response = self.read_message()
+        return self.response_interpret(response)
     
     def change_port(self, port):
         """
-        Function to change the port of an MXII valve. 
+        Function to change the port of the valve. 
         Input: 
             `port` (int): Port to change to
         Checks if the provided port is valid and checks the current port, it 
         change the port and sleep during the transition time. 
         
         """
+        #Check input
         if not isinstance(port, int) or port < 1 or port > self.ports:
-            raise ValueError('Invalid port number')
+            raise ValueError('Invalid port number: {}'.format(port))
 
-        ser = self.ser
-        while True:
-            self.read_message() #flush output   
-            self.write_message(self.message_builder('read'))
-            response1 = self.read_message()
-            #This should catch the only known error: the pump sending an empty byte.
-            #However, never tested because errors are extremely rare.
-            if response1 != b'':
-                break
-        current_port = self.response_interpret(response1)
-        if current_port[1] == port:
-            if self.verbose == True:
-                print('Valve: "{}" already in position {}'.format(self.name, port))
-
+        #Check if valve is already in correct position
+        current_port = self.get_port()
+        if current_port == port:
+            self.verboseprint('Valve: "{}" already in position {}'.format(self.name, port))
+        
+        #Change port
         else:
             while True:
                 self.write_message(self.message_builder('change', port))
                 self.wait_ready()
-                while True:
-                    self.read_message() #flush output
-                    #check current port after change
-                    self.write_message(self.message_builder('read'))
-                    response = self.read_message()
-                    port_val = re.compile(b'0.\\r')
-                    if re.match(port_val, response):
-                        break        
-                current_port = self.response_interpret(response)[1]
-                if self.verbose == True: 
-                    print('Valve: "{}" moved to port {}'.format(self.name, current_port))
-                if current_port == port:
+                current_port = self.get_port()
+                if  current_port == port:
+                    self.verboseprint('Valve: "{}" moved to port {}'.format(self.name, current_port))
                     break
-                    
